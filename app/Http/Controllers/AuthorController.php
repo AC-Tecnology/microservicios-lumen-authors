cad08e9a752373d901c1024bfee8464f95764f1e
gnuxdar <gnuxdar@gmail.com>
2021-03-09 19:24:07 -0500

    update Author controler with methods to CRUD

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;   //para accerder a los metodos response
 use App\Models\Author;
+use App\Traits\ApiResponser;    //para acceder a los metodos del trait
+use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;       //para acceder a los metodos del trait
     /**
      * Create a new controller instance.
      *
      * @return void
      */
+
     public function __construct()
     {
         //
@@ -21,44 +25,90 @@ class AuthorController extends Controller
      * Return authors List
      * @return Iluminate\Http\Response
      */
+
     public function index()
     {
-        // 
+        $authors = Author::all();
+        //return $authors;
+        return $this->successResponse($authors);
     }
 
     /**
      * Create a instance of Author
      * @return Iluminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name'      => 'required|max:255',
+            'gender'    => 'required|max:255|in:Male,Female',
+            'country'   => 'required|max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+
+        //HTTP_CREATED es una constante que devuelve 201 de creado en http
+        return $this->successResponse($author, Response::HTTP_CREATED);
     }
 
     /**
      * Return a specific authors
      * @return Iluminate\Http\Response
      */
+
     public function show($id)
     {
-        return User::findOrFail($id);
+        $author = Author::findOrFail($id);
+
+        return $this->successResponse($author);
     }
 
     /**
      * Update the informations of a author
      * @return Iluminate\Http\Response
      */
+
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'name'      => 'max:255',
+            'gender'    => 'max:255|in:Male,Female',
+            'country'   => 'max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::findOrFail($id);
+
+        $author->fill($request->all());
+
+        //si no hay cambios
+        if ($author->isClean()) {
+
+            //HTTP_CREATED es una conmtante que devuelve 422  que no puede ser procesada
+            return $this->errorResponse('Al menos un valor debe cambiar', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return $this->successResponse($author);
+
     }
 
     /**
      * Remove the informations of a author
      * @return Iluminate\Http\Response
      */
+
     public function destroy($id)
     {
-        //
+        $author = Author::findOrFail($id);
+
+        $author->delete();
+        
+        return $this->successResponse($author);
     }
 }
